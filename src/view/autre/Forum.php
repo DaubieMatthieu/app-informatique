0cbc2166f858c8665419838e099078748e270447
@daubim4 <daubim4@gmail.com>
2019-12-03 15:36:44 +0100

    Commit n°18 : finition page Forum.php/css

--- src/view/autre/Forum.php
+++ src/view/autre/Forum.php
@@ -28,7 +28,7 @@
 
 			<nav>
 				<h1> Forum </h1></br>
-				<form method="post" action="Forum.php">
+				<form method="post" action="Forum_admin.php">
 					<input type="search" name="search" autocomplete="off" placeholder="Rechercher un sujet" size="64" maxlength="64"/>
 				</form><br>
 				<button type='button' id='new_subject' onclick='show_new_subject_form()'>Nouveau sujet</button>
@@ -73,30 +73,45 @@
 				<div id="sujets">
 							<?php
 			      	while ($sujet=$rep->fetch()) {
-							?>
-              <div class='sujet'>
-                <a href="Forum_sujet.php?id_sujet=<?php echo $sujet['id_sujet'];?>"><button>
-                  <?php
-                  try {
-                    $id_utilisateur=$sujet['id_utilisateur'];
-                    $req2 = "SELECT nom, prenom FROM utilisateur where id_utilisateur=$id_utilisateur";
-                    $rep2 = $db->query($req2);
-                    $user=$rep2->fetch();
-                    ?>
-                    <h3><?php echo $sujet['titre']; ?></h3><p></br></p>
-                    <div class="utilisateur"><?php echo $user['prenom']." ".$user['nom'];?></div>
-										<div class="date"><?php echo date('d/m/Y H:i', strtotime($sujet['date_creation_sujet'])); ?></div>
-                    <?php
-                  } catch (Exception $e)
-                  {
-                    ?><script type="text/javascript">
-                    show_message("Erreur serveur au sujet id="+<?php echo $sujet['id_sujet'] ?>,'red');
-                    </script><?php
-                  }
-                ?>
-							</button></a>
-              </div>
-			      	<?php
+								$id_sujet=$sujet['id_sujet'];
+								?>
+	              <div class='sujet'>
+	                <a href="Forum_sujet_admin.php?id_sujet=<?php echo $id_sujet;?>"><button>
+	                  <?php
+	                  try {
+	                    $id_utilisateur=$sujet['id_utilisateur'];
+	                    $req2 = "SELECT nom, prenom FROM utilisateur where id_utilisateur=$id_utilisateur";
+	                    $rep2 = $db->query($req2);
+	                    $user=$rep2->fetch();
+											$titre_sujet=$sujet['titre'];
+											$date_creation_sujet=date('d/m/Y H:i', strtotime($sujet['date_creation_sujet']));
+											$prenom=$user['prenom'];
+											$nom=$user['nom'];
+	                    ?>
+	                    <h3><?php echo $titre_sujet; ?></h3><p></br></p>
+	                    <div class="utilisateur"><?php echo $prenom." ".$nom; ?></div>
+											<div class="date"><?php echo $date_creation_sujet; ?></div>
+	                    <?php
+										$titre_sujet=addslashes($titre_sujet);
+										$prenom=addslashes($prenom);
+										$nom=addslashes($nom);
+		                } catch (Exception $e)
+	                  {
+	                    ?><script type="text/javascript">
+	                    show_message("Erreur serveur au sujet id="+<?php echo $id_sujet ?>,'red');
+	                    </script><?php
+	                  }
+	                ?>
+								</button></a>
+								<?php
+								if ($_SESSION['role']=='A') {
+									?>
+									<div class="delete"><button onclick="show_delete_form(<?php echo "$id_sujet".','."'$titre_sujet'".','."'$date_creation_sujet'".','."'$prenom'".','."'$nom'" ; ?>)"></button></div>
+									<?php
+								}
+								?>
+								</div>
+				      	<?php
 							}
 							?>
 				</div>
@@ -105,6 +120,8 @@
 			?>
 
 		</section>
+
+		<!--formulaire création sujet-->
 
 		<div id="new_subject_form">
 			<form action="../../controller/create_new_subject.php" method="POST">
@@ -119,6 +136,40 @@
 				<input type="submit" class='confirm' value='Valider'><input type='button' class='cancel' value='Annuler' onclick="hide_new_subject_form()">
 			</form>
 		</div>
+
+		<!--formulaire suppression utilisateur-->
+
+
+		<?php
+		if ($_SESSION['role']=='A') {
+			?>
+			<div id="delete_form">
+				<form action="../../controller/admin_delete_subject.php" method="POST">
+					<h1>Supprimer</h1><br>
+
+					<label><b>Id du sujet:</b></label>
+					<input type="number" name="id_sujet" readonly='readonly' required>
+					<br><br>
+					<label><b>Titre du sujet :</b></label>
+					<input type="email" name="titre_sujet" readonly="readonly" required>
+					<br><br>
+					<label><b>Date de création du sujet :</b></label>
+					<input type="text" name="date_creation_sujet" readonly="readonly" required>
+					<br><br>
+					<label><b>Prénom de l'utilisateur :</b></label>
+					<input type="text" name="prenom" readonly="readonly" required>
+					<br><br>
+					<label><b>Nom de l'utilisateur :</b></label>
+					<input type="text" name="nom" readonly="readonly" required>
+					<br><br>
+					<p style="color:#59adde;text-align:center;">Voulez-vous vraiment supprimer ce sujet ?</p>
+					<br><br>
+					<input type="submit" class='confirm' value='Valider'><input type='button' class='cancel' value='Annuler' onclick="hide_delete_form()">
+				</form>
+			</div>
+			<?php
+		}
+		?>
 
 		<?php include("../general/Footer.php"); ?>
 
