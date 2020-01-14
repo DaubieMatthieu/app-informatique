8acc902b4726b14219f3946282880ab1f7250dea
Louis GONGORA <louisg.pro@gmail.com>
2020-01-14 15:21:47 +0100

    Home : Button to top

--- src/public/js/home/home.js
+++ src/public/js/home/home.js
@@ -17,7 +17,6 @@ window.onclick = function(event) {
   }
 }
 
-
 var slideIndex = 0;
 //showSlides(slideIndex);
 automaticSwitchSlide();
@@ -60,4 +59,25 @@ function automaticSwitchSlide() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].classList.add('hero-selected');
   setTimeout(automaticSwitchSlide, 6000); // Change image every 2 seconds
+}
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
