d696522d892b6ffc1f0bd9e922d61e8a7ef8f2c1
Nayteuh <nathan.longa60@gmail.com>
2020-01-26 12:25:50 +0100

    Remade CGU and mentions look

--- src/view/loggedout/CGU.php
+++ src/view/loggedout/CGU.php
@@ -2,36 +2,49 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<link rel="stylesheet" href="../../public/css/autre/modif_view.css">
+		<link rel="stylesheet" href="../../public/css/loggedout/CGU_Mentions.css">
 		<title> CGU </title>
-    </head>
+	</head>
+	
+	<?php session_start(); ?>
+
     <body>
-        <h1>Conditions générales d'utilisation</h1>
+		<?php include("../general/Header.php"); ?>
 
-        <?php
-            //connexion à la base de données
-			include('../../model/db_connect.php');
-			$db = db_connect();
-			if ($db===false) {
-				?><script> show_message('Connexion à la base de donnée impossible','red'); </script><?php
-				exit;
-            }
-            
-            try {
-				$req = "SELECT contenu FROM contenu_site WHERE type = 'CGU'";
+		<!--section messages serveur-->
+
+		<?php include('../general/Message_serveur.php');?>
+
+		<section>
+			<h1>Conditions générales d'utilisation</h1>
+
+			<?php
+				//connexion à la base de données
+				include('../../model/db_connect.php');
+				$db = db_connect();
+				if ($db===false) {
+					?><script> show_message('Connexion à la base de donnée impossible','red'); </script><?php
+					exit;
+				}
 				
-				$rep = $db->query($req);
-			} catch (Exception $e){
-				?><script type="text/javascript"> show_message('Erreur serveur','red'); </script><?php
-				exit;
-			}
+				try {
+					$req = "SELECT contenu FROM contenu_site WHERE type = 'CGU'";
+					
+					$rep = $db->query($req);
+				} catch (Exception $e){
+					?><script type="text/javascript"> show_message('Erreur serveur','red'); </script><?php
+					exit;
+				}
 
-			if ($rep->rowCount()==0){
-				?><script type="text/javascript"> show_message('Aucun contenu trouvé pour les CGU','orange'); </script><?php
-			} else {
-                $cgu = $rep->fetch();   
-                echo $cgu['contenu'];
-			}
-        ?>
+				if ($rep->rowCount()==0){
+					?><script type="text/javascript"> show_message('Aucun contenu trouvé pour les CGU','orange'); </script><?php
+				} else {
+					$cgu = $rep->fetch();   
+					echo nl2br($cgu['contenu']);
+				}
+
+				include("../general/Footer.php");
+			?>
+		</section>
     </body>
 </html>
--- src/view/loggedout/Mentions_legales.php
+++ src/view/loggedout/Mentions_legales.php
@@ -2,36 +2,49 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<link rel="stylesheet" href="../../public/css/autre/modif_view.css">
+		<link rel="stylesheet" href="../../public/css/loggedout/CGU_Mentions.css">
 		<title> Mentions légales </title>
-    </head>
+	</head>
+	
+	<?php session_start(); ?>
+
     <body>
-        <h1>Mentions légales</h1>
+		<?php include("../general/Header.php"); ?>
 
-        <?php
-            //connexion à la base de données
-			include('../../model/db_connect.php');
-			$db = db_connect();
-			if ($db===false) {
-				?><script> show_message('Connexion à la base de donnée impossible','red'); </script><?php
-				exit;
-            }
-            
-            try {
-				$req = "SELECT contenu FROM contenu_site WHERE type = 'Mentions légales'";
+		<!--section messages serveur-->
+
+		<?php include('../general/Message_serveur.php');?>
+
+		<section>
+			<h1>Mentions légales</h1>
+
+			<?php
+				//connexion à la base de données
+				include('../../model/db_connect.php');
+				$db = db_connect();
+				if ($db===false) {
+					?><script> show_message('Connexion à la base de donnée impossible','red'); </script><?php
+					exit;
+				}
 				
-				$rep = $db->query($req);
-			} catch (Exception $e){
-				?><script type="text/javascript"> show_message('Erreur serveur','red'); </script><?php
-				exit;
-			}
+				try {
+					$req = "SELECT contenu FROM contenu_site WHERE type = 'Mentions légales'";
+					
+					$rep = $db->query($req);
+				} catch (Exception $e){
+					?><script type="text/javascript"> show_message('Erreur serveur','red'); </script><?php
+					exit;
+				}
 
-			if ($rep->rowCount()==0){
-				?><script type="text/javascript"> show_message('Aucun contenu trouvé pour les mentions légales','orange'); </script><?php
-			} else {
-                $mentionsLgls = $rep->fetch();   
-                echo $mentionsLgls['contenu'];
-			}
-        ?>
+				if ($rep->rowCount()==0){
+					?><script type="text/javascript"> show_message('Aucun contenu trouvé pour les mentions légales','orange'); </script><?php
+				} else {
+					$mentionsLgls = $rep->fetch();   
+					echo nl2br($mentionsLgls['contenu']);
+				}
+
+				include("../general/Footer.php");
+			?>
+		</section>
     </body>
 </html>
